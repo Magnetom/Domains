c5c419fca8f17f59af189a98c55a306641f7dc71
Odyssey <odysseygroove@gmail.com>
2018-12-16 02:03:36 +0700

    Версия: 1.02 Изменения: незначительные улучшения.

--- dbConnect.php
+++ dbConnect.php
@@ -2,16 +2,17 @@
 
 //Defining Constants
 define('HOST','localhost');
-define('USER','root');
-define('PASS','');
 define('DB','sav');
+define('USER','user');
+define('PASS','mysqluser');
 
 // имя текуще БД.
 $DATABASE_NAME = DB;
 
 // Табилцы текущей БД.
-$MARKS_TABLE    = 'marks';
-$VEHICLES_TABLE = 'vehicles';
+$MARKS_TABLE     = 'marks';
+$VEHICLES_TABLE  = 'vehicles';
+$VARIABLES_TABLE = 'variables';
 
 //Connecting to Database
-$con=mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect to the database');
+$con=mysqli_connect(HOST,USER,PASS,DB);// or die('Unable to Connect to the database');
